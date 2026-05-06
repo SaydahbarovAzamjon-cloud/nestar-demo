b2f14cf3a968b14f192738ca2e42574ab3f8382a
Azamjon <iamsaidakbarov@gmail.com>
2026-05-06 21:57:45 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
+
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
@@ -14,9 +17,12 @@ export default function App({ Component, pageProps }: AppProps) {
     
     // Socket.io, Redux, Mui ...
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
             <Component {...pageProps} />
         </ThemeProvider>
+        </ApolloProvider>
+
     );
 }
